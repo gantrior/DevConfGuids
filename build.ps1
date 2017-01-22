2ec7c8a4e18ad43afcc610128093b56922144dba
Tomas Panik <tomas.panik@covered.lht>
2017-01-22 16:44:28 +0100

    added docker-compose

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 cd build
-docker build --build-arg HTTP_PROXY=%HTTP_PROXY% --build-arg HTTPS_PROXY=%HTTPS_PROXY% -t msbuild .
+docker build -t msbuild .
 cd ..
 
 Invoke-Expression "docker run -v $($PWD):c:/src msbuild"
 
 
-docker build -f Dockerfile -t guid-generator:0.1 .
+docker build -f Dockerfile -t devconfguid .
 
