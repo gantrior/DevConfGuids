e560f8f7e12cdbec37f6311b9bc11dcaaaf3f6de
Tomas Panik <tomas.panik@covered.lht>
2017-01-22 14:42:14 +0100

    fixed build

--- build/Build.ps1
+++ build/Build.ps1
@@ -2,4 +2,4 @@
 Invoke-Expression "nuget restore $((Get-Item "*.sln").FullName)" 
 
 # build
-Invoke-Expression "& `"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe`" $((Get-Item "*.sln").FullName) /p:DeployOnBuild=true /p:PublishProfile=Test /t:Clean,Build"
+Invoke-Expression "& `"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe`" $((Get-Item "*.sln").FullName) /p:DeployOnBuild=true /p:PublishProfile=Default /t:Clean,Build"
